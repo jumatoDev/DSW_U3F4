882f8854f5e20ef2b9cb8e0ea2f06a30fd61c210
jumatoDev <jule.galan7@gmail.com>
2023-05-26 14:32:48 -0500

    Commit 1

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+function procesarRespuestas() {
+    let total=5;
+    let puntos=0;
+
+    let myForm = document.forms["formulario"];
+    let respuestasCorrectas = ["b", "b", "a", "a", "a"];
+
+    for(let i = 1 ; i <= total; i++){
+        if(myForm["r" + i].value == null || myForm["r" + i].value == ''){
+            alert('Para "ENVIAR" tienes que responder todas las preguntas');
+            return false;
+        } else{
+            if(myForm["r" + i].value === respuestasCorrectas[i - 1])
+            puntos ++;
+        }
+    }
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = 'Obtuviste ' + puntos + ' puntos de ' + total + ' posibles.';
+    return false;
+}
